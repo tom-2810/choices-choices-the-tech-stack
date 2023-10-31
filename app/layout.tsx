d80a9185bf1ae83748082c08251c248a854c79f9
tom-2810 <tom.poortman@hva.nl>
2023-10-31 13:09:13 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={styles}>
+    <html lang="en">
       <body>
         <nav><Link href="/"><img src="/vervoerregio_amsterdam_logo.svg" alt="" /></Link></nav>
         {children}
