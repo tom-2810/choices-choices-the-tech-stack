52b8ce8810ef3d1c0c579f6a646325bc41c3b3ca
tom-2810 <tom.poortman@hva.nl>
2023-11-01 07:45:53 +0100

    router instead of link

---
+++ app/not-found.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import Link from "next/link";
+import { useRouter } from 'next/navigation'
+import styles from './not-found.module.css'
+
+export default function NotFound() {
+    const router = useRouter()
+    return (
+        <div className={styles.error}>
+            <h3><span>Oeps!</span> Er is iets mis gegaan.</h3>
+
+            <p>
+                De pagina die je zoekt, is helaas niet gevonden. Misschien heb je een verkeerde afslag genomen
+                of is er iets misgegaan tijdens het navigeren van onze tool. Probeer het opnieuw om op de juiste
+                pagina te belanden.
+            </p>
+            <button onClick={() => router.push('/')}>Terug naar home</button>
+        </div>
+    )
+}
